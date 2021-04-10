523f18dab6642d37d9c87ad7a62f9d069d98700e
Thomas Varano <thvarano@gmail.com>
2021-04-10 19:58:03 -0400

    add user profile page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -13,9 +14,84 @@ def test():
 
 @app.route('/profile', methods=['GET'])
 def profile():
-    return render_template('profile.html')
+    #  takes a user json
+    usr="""{
+    "id": "110110", 
+    "name": "Thomas", 
+    "email": "tvarano@umd.edu", 
+    "total_packages": 25, 
+    "total_carbon": 101, 
+    "total_miles": 1000
+    }"""
+
+    pkg="""[{
+    "id": "100100", 
+    "user_id": "100100", 
+    "tracking": "100100", 
+    "weight": 10.2, 
+    "length": 0.5, 
+    "width": 0.5, 
+    "height": 0.5, 
+    "volume": 0.125, 
+    "origin": "New York City, NY", 
+    "ship_date": "2020-11-05", 
+    "destination": "Seoul, South Korea", 
+    "transportation": "plane", 
+    "carbon": 102
+    }, 
+    {
+    "id": "100100", 
+    "user_id": "100100", 
+    "tracking": "100100", 
+    "weight": 10.2, 
+    "length": 0.5, 
+    "width": 0.5, 
+    "height": 0.5, 
+    "volume": 0.125, 
+    "origin": "New York City, NY", 
+    "ship_date": "2020-11-05", 
+    "destination": "Seoul, South Korea", 
+    "transportation": "plane", 
+    "carbon": 102
+    }, 
+    {
+    "id": "100100", 
+    "user_id": "100100", 
+    "tracking": "100100", 
+    "weight": 10.2, 
+    "length": 0.5, 
+    "width": 0.5, 
+    "height": 0.5, 
+    "volume": 0.125, 
+    "origin": "New York City, NY", 
+    "ship_date": "2020-11-05", 
+    "destination": "Seoul, South Korea", 
+    "transportation": "plane", 
+    "carbon": 102
+    }]"""
+
+    return render_template('profile.html', user=json.loads(usr), packages=json.loads(pkg))
+
+@app.route('/profile/packages')
+def moreinfo(): 
+    # query all package information
+    # list chronologically in table
+
+    # get json data 
+    return render_template('packages.html', data=dta)
+
+# @app.route('/login')
+# def login(): 
+# if user exists, just scrape
+# otherwise, add
 
 
+# def scrape(): 
+    # call gmail api scrape
+    # get the info as json
+    # save to db all the packages. 
+
+    # recalculate user statistics
 
 if __name__ == "__main__":
     app.run(debug=True)
