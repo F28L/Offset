550249eab4bea587704209b501c8f54abf16e6ea
Thomas Varano <thvarano@gmail.com>
2021-04-10 17:27:15 -0400

    unresponsive front page

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/test', methods=['GET'])
+def test():
+    return render_template('test.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
